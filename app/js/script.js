759c87b99e75056613ac5e75c29f082a4917fe0a
clarice_r <claricerdallorto@gmail.com>
2021-07-12 17:22:37 +0100

    Finished the animation fade in out for mobile menu bkg

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,20 @@
 const btnHamburger = document.querySelector('#btnHamburger');
-// const bgOverlay = document.querySelector('#bgOverlay')
+const overlay = document.querySelector('.overlay');
 const header = document.querySelector('.header')
+
 btnHamburger.addEventListener('click', function(){
+
     console.log('open hamburger');
-    if(header.classList.contains('open')){
+
+    if(header.classList.contains('open')){ // Close menu
         header.classList.remove('open');
-        // bgOverlay.classList.remove('overlay');
-    } else {
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+       
+    } else { // Open menu
         header.classList.add('open');
-        // bgOverlay.classList.add('overlay');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+
     }
 });
