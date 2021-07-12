a51f9f725144fa1c2ccbeba7abeac99326d4c9db
clarice_r <claricerdallorto@gmail.com>
2021-07-12 15:26:13 +0100

    Created hamburger menu animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
-console.log("hiya!")
+const btnHamburger = document.querySelector('#btnHamburger');
+// const bgOverlay = document.querySelector('#bgOverlay')
+const header = document.querySelector('.header')
+btnHamburger.addEventListener('click', function(){
+    console.log('open hamburger');
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+        // bgOverlay.classList.remove('overlay');
+    } else {
+        header.classList.add('open');
+        // bgOverlay.classList.add('overlay');
+    }
+});
